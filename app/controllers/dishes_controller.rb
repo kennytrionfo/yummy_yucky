dad3ec6ca2d1cbde922f2d5a34b7ef328e2d9916
Kenny T <ktrionfo@integracore.com>
2015-03-05 18:56:07 -0700

    added dish and form partials in dishes view

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,6 +1,22 @@
 class DishesController < ApplicationController
 
+	before_action :set_restaurant
 
+		def create
+			@dish = @restaurant.dish.create(dish_params)
+			redirect_to @restaurant 
+		end
+
+
+		private
+
+		def set_restaurant 
+			@restaurant = Restaurant.find(params[:restaurant_id])
+		end
+
+		def dish_params
+			params[:dish].permit(:content)
+		end
 
 
 end

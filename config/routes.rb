11f2bffdb04ba88f601907318652a611a21afa63
Kenny T <ktrionfo@integracore.com>
2015-03-05 18:39:13 -0700

    added resources for dishes and nested in restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants do 
+    resources :dishes
+  end
+
+
   root 'restaurants#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
